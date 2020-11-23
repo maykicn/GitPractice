70a5d1bae84d0724f2437ed640280c1a2fc9b55c
maykicn <maykic@gmail.com>
2020-11-23 22:25:32 +0200

    added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,3 +4,8 @@ public class AwesomeTest {
 //green means staging area
 // greay means igore area
 //color normal means it is not commited no changes
+
+// i just pulled the new change from Github that i did not have locally
+// and my project is in sync with remote repository
+
+// now i made awesome changes ans I AM ABOUT To push this back to GitHub
